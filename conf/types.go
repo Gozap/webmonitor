e38154f6d96291762e92bb92f36c1905df1557e2
mritd <mritd1234@gmail.com>
2019-01-24 11:48:00 +0800

    feat(target): add http code check
    add http code check
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- conf/types.go
+++ conf/types.go
@@ -19,7 +19,11 @@ package conf
 import (
 	"errors"
 	"io/ioutil"
+	"strconv"
+	"strings"
 	"time"
+
+	"github.com/sirupsen/logrus"
 
 	"gopkg.in/yaml.v2"
 )
@@ -37,8 +41,8 @@ func (cfg *Config) SetConfigPath(configPath string) {
 	cfg.configPath = configPath
 }
 
-// write config to yaml file
-func (cfg Config) Write() error {
+// write config
+func (cfg *Config) Write() error {
 	if cfg.configPath == "" {
 		return errors.New("config path not set")
 	}
@@ -49,17 +53,34 @@ func (cfg Config) Write() error {
 	return ioutil.WriteFile(cfg.configPath, out, 0644)
 }
 
-// load config from yaml file
-func (cfg *Config) Load(filePath string) error {
+// write config to yaml file
+func (cfg *Config) WriteTo(filePath string) error {
 	if filePath == "" {
 		return errors.New("file path is empty")
 	}
 	cfg.configPath = filePath
-	buf, err := ioutil.ReadFile(filePath)
+	return cfg.Write()
+}
+
+// load config
+func (cfg *Config) Load() error {
+	if cfg.configPath == "" {
+		return errors.New("config path not set")
+	}
+	buf, err := ioutil.ReadFile(cfg.configPath)
 	if err != nil {
 		return err
 	}
 	return yaml.Unmarshal(buf, cfg)
+}
+
+// load config from yaml file
+func (cfg *Config) LoadFrom(filePath string) error {
+	if filePath == "" {
+		return errors.New("file path is empty")
+	}
+	cfg.configPath = filePath
+	return cfg.Load()
 }
 
 func (cfg *Config) Default() {
@@ -92,13 +113,50 @@ type Basic struct {
 }
 
 type Target struct {
-	Name    string        `yaml:"name"`
-	Address string        `yaml:"address"`
-	TimeOut time.Duration `yaml:"timeout"`
-	Method  string        `yaml:"method"`
-	Payload string        `yaml:"payload"`
-	Proxy   string        `yaml:"proxy"`
-	Cron    string        `yaml:"cron"`
+	Name          string        `yaml:"name"`
+	Address       string        `yaml:"address"`
+	TimeOut       time.Duration `yaml:"timeout"`
+	Method        string        `yaml:"method"`
+	PassCode      []string      `yaml:"pass_code"`
+	ResponseCheck []string      `yaml:"response_check"`
+	Payload       string        `yaml:"payload"`
+	Proxy         string        `yaml:"proxy"`
+	Cron          string        `yaml:"cron"`
+}
+
+func (t Target) CheckCode(code int) bool {
+	for _, cs := range t.PassCode {
+		if strings.Count(cs, "-") == 1 && len(strings.Split(cs, "-")) == 2 {
+			codes := strings.Split(cs, "-")
+			begin, err := strconv.Atoi(codes[0])
+			if err != nil {
+				logrus.Errorf("target [%s] passcode [%s] invalid!", t.Name, cs)
+				return false
+			}
+			end, err := strconv.Atoi(codes[1])
+			if err != nil {
+				logrus.Errorf("target [%s] passcode [%s] invalid!", t.Name, cs)
+				return false
+			}
+
+			if code >= begin && code <= end {
+				return true
+			}
+
+		} else {
+			passCode, err := strconv.Atoi(cs)
+			if err != nil {
+				logrus.Errorf("target [%s] passcode [%s] invalid!", t.Name, cs)
+				return false
+			}
+
+			if code == passCode {
+				return true
+			}
+		}
+	}
+
+	return false
 }
 
 type Targets []Target
@@ -122,12 +180,14 @@ func Example() Config {
 		},
 		Targets: []Target{
 			{
-				Name:    "百度",
-				Address: "https://baidu.com",
+				Name:     "百度",
+				PassCode: []string{"200-300", "404"},
+				Address:  "https://baidu.com",
 			},
 			{
-				Name:    "漠然",
-				Address: "https://mritd.me",
+				Name:     "漠然",
+				PassCode: []string{"200-300", "404"},
+				Address:  "https://mritd.me",
 			},
 		},
 	}
